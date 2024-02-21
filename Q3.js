0d41c3dd2db5ae5df2ba324e9c46e72e9e3f97b5
Areej11002 <160529992+Areej11002@users.noreply.github.com>
2024-02-21 20:48:45 +0500

    Add files via upload

---
+++ Q3.js
@@ -0,0 +1,6 @@
+"use strict";
+// 3. Name cases: store a person's name in a variable, and then print that person's name in lowcase, uppercase, and titlecase
+let firstName = "Areej Shah";
+//console.log(firstName.toLocaleLowerCase());
+//console.log(firstName.toLocaleUpperCase());
+//console.log(firstName.charAt(0).toLocaleUpperCase() + firstName.slice(1));
